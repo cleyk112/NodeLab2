600417305144e95e7913cb7519cfc00b0da31129
cleyk112 <cleyk112@gmail.com>
2024-01-04 17:54:26 +0200

    examplary post method for json handler

--- handlers/jsonHandler.js
+++ handlers/jsonHandler.js
@@ -2,6 +2,9 @@ export function handleJSON(req, res) {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "JSON response" }));
+  }else if (req.method === "POST"){
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "JSON response to a POST method implementation" }));
   } else {
     res.writeHead(405, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Method Not Allowed" }));
