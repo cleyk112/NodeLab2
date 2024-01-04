c29d2b9677144b3d9f630f6f5804143d6a5fca1f
cleyk112 <cleyk112@gmail.com>
2024-01-04 16:52:02 +0200

    added router, routes and handlers for 2 formats

--- handlers/index.js
+++ handlers/index.js
@@ -1,4 +1,4 @@
 export function handleIndex(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Welcome to the homepage!");
+  res.end("Welcome to the homepage! try adding /xml or /json to see the two possible routes");
 }
--- handlers/xmlHandler.js
+++ handlers/xmlHandler.js
@@ -1,7 +1,7 @@
 export function handleXML(req, res) {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/xml" });
-    res.end("<response><message>XML response</message></response>");
+    res.end("<response><message>This is an example of an XML message</message></response>");
   } else {
     res.writeHead(405, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Method Not Allowed" }));
