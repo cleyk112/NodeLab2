6a8c9a654613468fac086de30ec9769ed1be26e0
cleyk112 <cleyk112@gmail.com>
2024-01-04 17:10:32 +0200

    added options method to xml handler

--- handlers/xmlHandler.js
+++ handlers/xmlHandler.js
@@ -2,6 +2,9 @@ export function handleXML(req, res) {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/xml" });
     res.end("<response><message>This is an example of an XML message</message></response>");
+  } else if (req.method === 'OPTIONS'){
+    res.writeHead(205, {"Content-Type": "application/xml"});
+    res.end("<response><message>OPTIONS</message></response>")
   } else {
     res.writeHead(405, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ error: "Method Not Allowed" }));
